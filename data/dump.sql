f55b3af7fccd0f8894bd8bc53f38d1a147d4f6c2
Taras <37515849+ktv1@users.noreply.github.com>
2020-03-24 19:42:39 +0200

    Add sql.dump

---
+++ data/dump.sql
@@ -0,0 +1,22 @@
+-- --------------------------------------------------------
+-- Сервер:                       127.0.0.1
+-- Версія сервера:               5.6.43 - MySQL Community Server (GPL)
+-- ОС сервера:                   Win32
+-- HeidiSQL Версія:              10.2.0.5599
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Дані для експорту не вибрані
+
+-- Дані для експорту не вибрані
+
+-- Дані для експорту не вибрані
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
